930f1b9b3dfa8401e6d24958dad1fcae840444c7
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-06-30 12:14:27 +0530

    Delete Ex - 5.py

--- Ex - 5.py
+++
@@ -1,9 +0,0 @@
-from PyTrackRun.PyTrackRunnerClass import PyTrackRunner
-
-
-ptr = PyTrackRunner()
-ptr.pytrack_runner(UI_file_name = 'User_ImageUI_EscExit',flagUI = True,flagPupilTracking = True, 
-                eyeTrackingLog = True, eyeTrackingFileName = 'User_4', 
-                flagAudioRecorder = True, audioName = 'audio4',videoName = 'video', 
-                flagVideoRecorder = True, flagSyncAudioVideo = True)
-

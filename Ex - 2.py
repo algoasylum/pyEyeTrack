688ae09b051bf9c4bfead4e2757cc83669dca74b
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-06-30 12:13:54 +0530

    Delete Ex - 2.py

--- Ex - 2.py
+++
@@ -1,7 +0,0 @@
-from PyTrackRun.PyTrackRunnerClass import PyTrackRunner
-
-
-ptr = PyTrackRunner()
-ptr.pytrack_runner(UI_file_name = 'User_ImageUI_Blinking',flagUI = True,flagBlinkDetection = True, 
-                eyeTrackingLog = True, eyeTrackingFileName= 'User_2')
-

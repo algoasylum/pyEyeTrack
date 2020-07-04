81cc55ae51452471e69955ef55df9a38c0a40c8c
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-07-04 20:41:01 +0530

    Delete Ex - 3.py

--- Ex - 3.py
+++
@@ -1,7 +0,0 @@
-from PyTrackRunnerClass import PyTrackRunner
-
-
-ptr = PyTrackRunner()
-ptr.pytrack_runner(UI_file_name = 'slots',flagUI = True,flagBlinkDetection = True, 
-                eyeTrackingLog = False)
-

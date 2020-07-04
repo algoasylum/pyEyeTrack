3808518fc7a1eb076127ae74ead114451dc2c7a5
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-07-04 20:40:42 +0530

    Delete Ex - 1.py

--- Ex - 1.py
+++
@@ -1,6 +0,0 @@
-from PyTrackRunnerClass import PyTrackRunner
-
-ptr = PyTrackRunner()
-ptr.pytrack_runner(UI_file_name = 'SampleTextUI',flagUI = True,flagPupilTracking = True, 
-                eyeTrackingLog = True, eyeTrackingFileName= 'User_1')
-

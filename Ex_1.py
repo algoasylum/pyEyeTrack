3baa4542155defd4caca966b0122e2eac6bd7128
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-07-04 20:40:25 +0530

    Add files via upload

---
+++ Ex_1.py
@@ -0,0 +1,5 @@
+from PyTrackRunnerClass import PyTrackRunner
+
+ptr = PyTrackRunner()
+ptr.pytrack_runner(UI_file_name = 'Ex_1_SampleTextUI',flagUI = True,flagPupilTracking = True, 
+                eyeTrackingLog = True, eyeTrackingFileName= 'User_1')
---
+++ Ex_2.py
@@ -0,0 +1,7 @@
+from PyTrackRunnerClass import PyTrackRunner
+
+
+ptr = PyTrackRunner()
+ptr.pytrack_runner(UI_file_name = 'Ex_2_ImageUI',flagUI = True,flagBlinkDetection = True, 
+                eyeTrackingLog = True, eyeTrackingFileName= 'User_2')
+

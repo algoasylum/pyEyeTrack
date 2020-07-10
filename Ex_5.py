a152a2599735303d2efbcfc7ee19c4c815232f88
kimaya2 <50978003+kimaya2@users.noreply.github.com>
2020-07-10 20:20:49 +0530

    Update Ex_5.py

--- Ex_5.py
+++ Ex_5.py
@@ -5,10 +5,6 @@ ptr = PyTrackRunner()
 ptr.pytrack_runner(
     UI=True,
     UI_file_name='Ex_5_ImageUI_EscExit',
-    pupilTracking=True,
-    blinkDetection=True,
-    eyeTrackingLog=True,
-    eyeTrackingFileName='User_4',
     audioRecorder=True,
     audioName='audio4',
     videoName='video4',

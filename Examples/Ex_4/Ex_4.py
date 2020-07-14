04453349d805b6cdba61bbb1debf3c3b1be63138
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-07-14 10:42:07 +0530

    Updated flag

--- Examples/Ex_4/Ex_4.py
+++ Examples/Ex_4/Ex_4.py
@@ -4,6 +4,7 @@ from PyTrackRunnerClass import PyTrackRunner
 ptr = PyTrackRunner()
 ptr.pytrack_runner(
     pupilTracking=True,
+    blinkDetection=True,
     video_source=r"C:\Users\Kanchan\Downloads\Text_Reading\data\kanchan2\av.mkv",
     eyeTrackingLog=True,
     eyeTrackingFileName='User_3')

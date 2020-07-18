41fd75881571626ac48dc64af2d8abc92ae72669
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-07-18 12:49:44 +0530

    Created README.md

--- Examples/Ex_4/Ex_4.py
+++ Examples/Ex_4/Ex_4.py
@@ -5,6 +5,6 @@ ptr = PyTrack()
 ptr.pytrack_runner(
     pupilTracking=True,
     blinkDetection=True,
-    video_source=r"C:\Users\Kanchan\Downloads\Text_Reading\data\kanchan2\av.mkv",
+    video_source=r"#add path of the video input",
     eyeTrackingLog=True,
     eyeTrackingFileName='User_3')

1649632049fc1f49e3995c2f403eb169bc283969
Neha Chaudhari <nehac4371@gmail.com>
2020-07-22 12:56:17 +0530

    edited setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'PyEyeTrack',         
   packages = ['PyEyeTrack'],   
-  version = '0.3.5',      
+  version = '0.3.6',      
   license='MIT',        
   description = """PyEyeTrack is a python-based pupil-tracking library. The library tracks eyes with the commodity webcam 
                 and gives a real-time stream of eye coordinates. It provides the functionality of eye-tracking and 
@@ -12,12 +12,12 @@ setup(
   authors = 'Kanchan Sarolkar, Kimaya Badhe, Neha Chaudhari, Samruddhi Kanhed and Shrirang Karandikar',                   
   author_email = 'pytracklibrary@gmail.com',      
   url = 'https://github.com/algoasylum/PyEyeTrack',  
-  download_url = 'https://github.com/algoasylum/pyEyeTrack/archive/v_03_5.tar.gz',    
+  download_url = 'https://github.com/algoasylum/pyEyeTrack/archive/v_03_6.tar.gz',    
   keywords = ['Eye Tracking','blink detection','User Interface','Webcamera'], 
   install_requires=[
   'dlib==19.4',
-  'keyboard==0.13.3=pypi_0',
-  'numpy==1.18.1=pypi_0',
+  'keyboard==0.13.3',
+  'numpy==1.18.1',
   'opencv==3.3.1',
   'pandas==0.24.0',
   'pyaudio==0.2.11',
